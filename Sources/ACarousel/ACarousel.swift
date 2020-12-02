6d4e59b4f15c8db7f8bd26ae775b6db9041d5c23
Ajaz Ahmed <72081378+AjazDev@users.noreply.github.com>
2020-12-02 14:34:54 +0500

    Update ACarousel.swift

--- Sources/ACarousel/ACarousel.swift
+++ Sources/ACarousel/ACarousel.swift
@@ -103,7 +103,7 @@ extension ACarousel {
         self._sidesScaling = sidesScaling
         self._autoScroll = autoScroll
         self.content = content
-        self.$_focusedItem = focusedItem
+        self.$focusedItem = focusedItem
         if !self.isWrap {
             aState = AState(activeItem: 0)
         }
