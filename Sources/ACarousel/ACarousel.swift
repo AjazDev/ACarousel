de13c229db5ee42c34fef3d16836564df02c6bf6
Ajaz Ahmed <72081378+AjazDev@users.noreply.github.com>
2020-12-02 14:34:10 +0500

    Update ACarousel.swift

--- Sources/ACarousel/ACarousel.swift
+++ Sources/ACarousel/ACarousel.swift
@@ -42,7 +42,7 @@ public struct ACarousel<Data, Content> : View where Data : RandomAccessCollectio
     private let content: (Data.Element) -> Content
     
     private var timer: TimePublisher? = nil
-    @Binding private var _focusedItem: Int
+    @Binding private var focusedItem: Int
     
     @ObservedObject private var aState = AState()
     
